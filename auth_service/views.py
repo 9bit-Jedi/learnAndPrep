259affb8dffb07017fb53e6680dd3ed9a04f2340
Utsah <107045013+9bit-Jedi@users.noreply.github.com>
2024-06-10 00:02:50 +0530

    Delete auth_service directory (finally)

--- auth_service/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from rest_framework import generics, status
-
-# Create your views here.
-def index(request):
-    return HttpResponse("This will be auth api endpoint")
