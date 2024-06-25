ffed54b15c5d49f77d5f0fd4b7cf5863e924c679
Utsah <utsah@ch.iitr.ac.in>
2024-06-25 18:10:33 +0530

    Bug Fix : Unique constraint removed from Quiz

--- quiz/models.py
+++ quiz/models.py
@@ -9,7 +9,7 @@ from questions.models import Chapter, Question, AnswerSmcq, AnswerMmcq, AnswerIn
 
 class Quiz(models.Model):
   id = models.CharField(max_length=50, primary_key=True)
-  chapter_id = models.OneToOneField(Chapter, on_delete=models.CASCADE)
+  chapter_id = models.ForeignKey(Chapter, on_delete=models.CASCADE)
   user_id = models.ForeignKey(User, on_delete=models.CASCADE)
   
   module_total_questions = models.IntegerField()
@@ -28,7 +28,7 @@ class Quiz(models.Model):
     self.module_total_questions=Question.objects.filter(chapter_id=self.chapter_id, source='MODULE').count()
     self.main_total_questions=Question.objects.filter(chapter_id=self.chapter_id, source='MAIN').count()
     self.adv_total_questions=Question.objects.filter(chapter_id=self.chapter_id, source='ADV').count()
-    self.id = f"{self.chapter_id.id}Q{self.user_id.id}"
+    self.id = f"{self.user_id.id}Q{self.chapter_id.id}"
     super().save(*args, **kwargs)
   
   def __str__(self):
@@ -37,7 +37,7 @@ class Quiz(models.Model):
 class QuizQuestion(models.Model):
   quiz_id = models.ForeignKey(Quiz, on_delete=models.CASCADE)
   question_id = models.ForeignKey(Question, on_delete=models.CASCADE)
-  solved_status = models.BooleanField(default=False)
+  solved_status = models.BooleanField(default=True)
   
   def __str__(self):
       return f"Quiz {self.quiz_id} | Question {self.question_id.id}"
@@ -45,17 +45,20 @@ class QuizQuestion(models.Model):
 # attempt models
 
 class QuizQuestionAttemptAbstract(models.Model):
-  id = models.CharField(max_length=10, primary_key=True)
+  id = models.CharField(max_length=20, primary_key=True)
   quiz_question_id = models.ForeignKey(QuizQuestion, on_delete=models.CASCADE)
   is_correct = models.BooleanField(null=False)
   
   def save(self, *args, **kwargs):
-    self.id = f"{self.quiz_question_id.question_id.id}AT{self.quiz_question_id.quiz_id.id}"
+    self.id = f"{self.quiz_question_id.quiz_id.user_id.id}AT{self.quiz_question_id.question_id.id}"
     super().save(*args, **kwargs)
   
   class Meta:
     abstract=True
 
+  def __str__(self):
+      return f"Quiz {self.quiz_question_id.quiz_id} | Answer {self.quiz_question_id.question_id.id}"
+    
 class QuizQuestionAttemptSmcq(QuizQuestionAttemptAbstract):
   OPTION_CHOICES = [('A', 'A'),('B', 'B'),('C', 'C'),('D', 'D'),]
   marked_option = models.CharField(max_length=1,choices=OPTION_CHOICES)  
