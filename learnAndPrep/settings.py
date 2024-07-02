b27c74cdc0fe030db6530aba23af3ffe46b46041
Utsah <utsah@ch.iitr.ac.in>
2024-07-02 22:30:20 +0530

    refresh rotation true (will get new refresh+access token pair on refresh endpoint ; minor changes in .env and settings

--- learnAndPrep/settings.py
+++ learnAndPrep/settings.py
@@ -24,8 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-b*p$)e#h++vii7j^zm3gw-^4%7z__7sv1b64%eyf8rg+7krgm_'
-
+SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
@@ -169,28 +168,32 @@ AUTH_USER_MODEL = 'accounts.User'
 PASSWORD_RESET_TIMEOUT = 900
 
 # EMAIL CONFIGURATTION
+
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.office365.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = config('EMAIL_USER')
-EMAIL_HOST_PASSWORD = config('EMAIL_PASSWORD')
+
+OTP_EMAIL_USER = config('OTP_EMAIL_USER')
+OTP_EMAIL_PASSWORD = config('OTP_EMAIL_PASSWORD')
 EMAIL_USE_TLS = True
 
 DEFAULT_FROM_EMAIL = config('EMAIL_USER')
 
-# TWILLO CONFIGURATION
-# TWILIO_ACCOUNT_SID = config('TWILIO_ACCOUNT_SID')
-# TWILIO_AUTH_TOKEN = config('TWILIO_AUTH_TOKEN')
-# TWILIO_PHONE_NUMBER = config('TWILIO_PHONE_NUMBER')
+# CONTACT US CONFIGURATION
 
-# WHATSAPP CONFIGURATION
+# CONTACT_EMAIL_USER = config('CONTACT_EMAIL_USER')
+# CONTACT_EMAIL_PASSWORD = config('CONTACT_EMAIL_PASSWORD')
+
+# WHATSAPP OTP CONFIGURATION
+
 PHONE_NUMBER_ID = config('PHONE_NUMBER_ID')
 WHATSAPP_AUTH_TOKEN = config('WHATSAPP_AUTH_TOKEN')
 
+
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=40),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=1),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=30),
-    'ROTATE_REFRESH_TOKENS': False,
+    'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': True,
     'ALGORITHM': 'HS256',
     'SIGNING_KEY': SECRET_KEY,
