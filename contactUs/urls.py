07d8a39a9cfd1fa6a1cd1f53203b3a5d483660ef
Utsah <utsah@ch.iitr.ac.in>
2024-07-06 03:22:34 +0530

    removed dev branch files from stage

--- contactUs/urls.py
+++
@@ -1,8 +0,0 @@
-# accounts/urls.py
-from django.urls import path
-from .views import ContactusView
-
-urlpatterns = [
-    # Other URL patterns
-    path('send/', ContactusView.as_view(), name='contact-us'),
-]
