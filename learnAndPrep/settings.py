2f348c3aaef51e65705b6068bb6f16548d4abe79
Utsah <utsah@ch.iitr.ac.in>
2024-07-06 22:33:22 +0530

    migrated to remote-hosted neon postgres db (sqlite still present)

--- learnAndPrep/settings.py
+++ learnAndPrep/settings.py
@@ -96,10 +96,21 @@ WSGI_APPLICATION = 'learnAndPrep.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # },
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('PGDATABASE'),
+        'USER': config('PGUSER'),
+        'PASSWORD': config('PGPASSWORD'),
+        'HOST': config('PGHOST'),
+        'PORT': config('PGPORT', 5432),
+        'OPTIONS': {
+            'sslmode': 'require',
+        },
+  }
 }
 
 
